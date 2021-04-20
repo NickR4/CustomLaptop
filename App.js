34c144cc7c696a1d912357ea08f1d3d4850250d4
NickR4 <72036401+NickR4@users.noreply.github.com>
2021-04-19 19:58:26 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import Customize from './Customize';
+import Summary from './Summary';
+import Head from './Head';
+import './App.css';
+
+const FEATURES = {
+  Processor: [
+    {
+      name: '17th Generation Intel Core HB (7 Core with donut spare)',
+      cost: 700
+    },
+    {
+      name: 'Professor X AMD Fire Breather with sidewinder technology',
+      cost: 1200
+    }
+  ],
+  "Operating System": [
+    {
+      name: 'Ubuntu Linux 16.04',
+      cost: 200
+    },
+    {
+      name: 'Bodhi Linux',
+      cost: 300
+    }
+  ],
+  "Video Card": [
+    {
+      name: 'Toyota Corolla 1.5v',
+      cost: 1150.98
+    },
+    {
+      name: 'Mind mild breeze 2000',
+      cost: 1345
+    }
+  ],
+  Display: [
+    {
+      name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+      cost: 1500
+    },
+    {
+      name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+      cost: 1400
+    },
+  ]
+};
+
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  };
+
+  handleFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Head />
+        <main>
+          <Customize features={FEATURES} selected={this.state.selected} handleFeature={this.handleFeature} />
+          <Summary selected={this.state.selected} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
